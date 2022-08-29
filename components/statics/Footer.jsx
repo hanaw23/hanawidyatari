bde84e9bdb740ac90e60809f00ff48ed428e69d5
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-29 12:41:43 +0700

    update

--- components/statics/Footer.jsx
+++ components/statics/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <div className="bg-transparent text-gray-50 mt-8 ">
+    <div className="bg-transparent dark:text-gray-50 mt-8 ">
       <div className="flex flex-col ">
-        <hr className="w-[1050px] ml-60 mx-auto " />
+        <hr className="w-[1050px] ml-60 mx-auto dark:fill-gray-50 " />
         <div className="mt-3">
           <p className="font-semibold text-xs ml-60">© 2022 Hana Widyatari. All rights reserved. | Personal portfolio of work.</p>
         </div>
--- pages/experiences/index.jsx
+++ pages/experiences/index.jsx
@@ -2,7 +2,7 @@ import { detailExp } from "../../utility/experiences";
 
 export default function Experiences() {
   return (
-    <div className="h-fit bg-gradient-to-tr from-[#344ddc] to-rose-600 pb-10">
+    <div className="h-fit bg-gradient-to-tr from-[#344ddc] to-rose-600 pb-10 text-white">
       <div className="flex justify-center mb-8">
         <h1 className="mt-10 text-[40px] font-semibold">Work Experiences</h1>
       </div>
@@ -11,7 +11,7 @@ export default function Experiences() {
         <div className="grid grid-cols-2 gap-8 ">
           {detailExp.map((item, i) => {
             return (
-              <div className=" mx-auto border border-transparent backdrop-blur-md dark:bg-white/30 bg-gray-600/30 rounded-[10px] py-4 px-4 h-[320px] cursor-pointer hover:scale-125 hover:z-40 ml-40">
+              <div className=" mx-auto border border-transparent backdrop-blur-md bg-white/30 rounded-[10px] py-4 px-4 h-[320px] cursor-pointer hover:scale-125 hover:z-40 ml-40">
                 <div key={i} className="text-center">
                   <div>
                     <h1 className="font-semibold text-[25px]">{item.role}</h1>
