4184242efe2a9fe3316995ee2676da7ae8400b47
Hana Widyatari <77064807+hanaw23@users.noreply.github.com>
2023-11-30 20:18:38 +0700

    fix: handle width

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,30 +8,24 @@ import ContactMe from "./contactMe";
 
 export default function Home() {
   return (
-    <>
+    <div className="w-full">
       <ScrollProgress />
-
       <Navbar />
-
       <section id="home">
         <HomePage />
       </section>
-
       <section id="aboutMe">
         <AboutMe />
       </section>
-
       <section id="experiences">
         <Experiences />
       </section>
-
       <section id="projects">
         <Projects />
       </section>
-
       <section id="contactMe">
         <ContactMe />
       </section>
-    </>
+    </div>
   );
 }
