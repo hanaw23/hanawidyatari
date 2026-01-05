f1d51fb585eaa04c316b0f1673c9ec12427727c6
Hana Widyatari <77064807+hanaw23@users.noreply.github.com>
2026-01-05 11:09:30 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const withSass = require("@zeit/next-sass");
 
 module.exports = withSass({
   cssModules: true,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ["image/webp"],
+    minimumCacheTTL: 60,
+  },
   webpack(config) {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
@@ -28,12 +34,6 @@ module.exports = {
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ["image/webp"],
-    minimumCacheTTL: 60,
-  },
 };
 
 module.exports = nextConfig;
